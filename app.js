5ee1e9f8bba16302f2d0052aad34d752579fd198
chau-duong <chau_duong99@yahoo.com.au>
2016-08-25 15:30:04 +0930

    Update app.js

--- app.js
+++ app.js
@@ -168,7 +168,7 @@ app.use(function(err, req, res, next)
 });
 
 //var server = app.listen(8080, function()
-var server= http.createServer(app).listen(process.env.PORT || 3000)
+var server = app.listen(process.env.PORT || 3000, function()
 {
     console.log("listening on port " + server.address().port);
 });
